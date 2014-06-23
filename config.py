f5d2a6071df9ebd23ca4d52799da55e5ff22ac95
Kevin Roos <kevin.roos@web.de>
2014-06-23 18:02:58 +0200

    - Removed semicolons - Doccomments now with double colons - Removed unneccesary parentheses - Functions: snake_casing instead of camelCasing - Variables: snake_casing insteaf of camelCasing - Renamed method parameter albumTitle to albumtitle

--- config.py
+++ config.py
@@ -2,19 +2,19 @@ __author__ = 'Rookev'
 import os
 
 # Program identification for Reddit
-user_agent = "Reddit Image Scraper by /u/Ronny93";
+useragent = "Reddit Image Scraper by /u/Ronny93"
 
 # Target user name
-username = "";
+username = ""
 
 # Number of posts to scrape
-limit = 0;
+limit = 0
 
 # Path to user directory
 userhome = os.path.expanduser('~')
 
 # Path to user desktop
-folderName = userhome + os.path.sep + "Desktop" + os.path.sep;
+foldername = userhome + os.path.sep + "Desktop" + os.path.sep
 
 # Counter for downloaded images
-currentFile = 1;
+currentfile = 1
--- downloader.py
+++ downloader.py
@@ -8,124 +8,123 @@ from bs4 import BeautifulSoup
 import config
 
 
-def processDirectLink(url, folder):
-    '''
+def process_direct_link(url, folder):
+    """
     Downloads an imgur image from a direct link ending on "jpg", "png" or "gif"
 
     :param url: "http://i.imgur.com/XYZ123.jpg"
     :param folder: Name of folder where image is saved
-    '''
+    """
 
     # Initializing file format
-    fileFormat = "";
+    fileformat = ""
 
     # If applicable, remove "?1" at the end of the url
-    if ("?" in url):
+    if "?" in url:
         url = url[:url.find("?")]
 
     # Set file format
-    if (url.endswith(".jpg")):
-        fileFormat = ".jpg";
+    if url.endswith(".jpg"):
+        fileformat = ".jpg"
 
-    elif (url.endswith(".png")):
-        fileFormat = ".png";
+    elif url.endswith(".png"):
+        fileformat = ".png"
 
-    elif (url.endswith(".gif")):
-        fileFormat = ".gif";
+    elif url.endswith(".gif"):
+        fileformat = ".gif"
 
     # File format was found
-    if (fileFormat != ""):
+    if fileformat != "":
 
         # Generate file name: /[Folder]/[Index].[Format]
-        fileName = folder + str(config.currentFile) + fileFormat;
+        filename = folder + str(config.currentfile) + fileformat
 
         # Download file
-        print("Downloading image " + str(config.currentFile) + ":\t" + url);
+        print("Downloading image " + str(config.currentfile) + ":\t" + url)
         try:
-            urllib.request.urlretrieve(url, fileName);
+            urllib.request.urlretrieve(url, filename)
         except:
-            print("Error while opening:\t" + url);
-            return;
+            print("Error while opening:\t" + url)
+            return
 
         # Increment counter to generate new file name for the next download
-        config.currentFile += 1;
+        config.currentfile += 1
 
 
-def processNormalLink(url, folder):
-    '''
+def process_normal_link(url, folder):
+    """
     Processes an imgur link by extracting the direct link and downloading it
 
     :param url: "http://imgur.com/XYZ123"
     :param folder: Name of folder where image is saved
-    '''
+    """
 
     # Read html file to url
     try:
-        htmlPage = urllib.request.urlopen(url);
+        htmlpage = urllib.request.urlopen(url)
     except:
-        print("Error while opening:\t" + url);
-        return;
+        print("Error while opening:\t" + url)
+        return
     try:
-        soup = BeautifulSoup(htmlPage);
+        soup = BeautifulSoup(htmlpage)
     except:
-        print("Error while parsing:\t" + url);
-        return;
+        print("Error while parsing:\t" + url)
+        return
 
     # Extract url from html file
-    url = soup.select('.image a');
+    url = soup.select('.image a')
 
-    if (len(url) == 1):
-        url = url[0]['href'];
+    if len(url) == 1:
+        url = url[0]['href']
 
         # Download image
-        url = formatUrl(url);
-        processDirectLink(url, folder);
+        url = format_url(url)
+        process_direct_link(url, folder)
 
-    # lastSlash = url.rfind("/");
 
-def processAlbum(url, albumTitle):
-    '''
+def process_album(url, albumtitle):
+    """
     Processes an imgur album by extracting direct links and downloading them seperately.
 
     :param url: "http://imgur.com/a/XYZ123"
-    :param albumTitle: Name of subfolder to be created for this album
-    '''
+    :param albumtitle: Name of subfolder to be created for this album
+    """
 
     # Read html file to url
     try:
-        htmlPage = urllib.request.urlopen(url);
+        htmlpage = urllib.request.urlopen(url)
     except:
-        print("Error while opening:\t" + url);
-        return;
+        print("Error while opening:\t" + url)
+        return
     try:
-        soup = BeautifulSoup(htmlPage);
+        soup = BeautifulSoup(htmlpage)
     except:
-        print("Error while parsing:\t" + url);
-        return;
+        print("Error while parsing:\t" + url)
+        return
 
     # Generate subfoldername for album
-    subfolder = config.folderName + albumTitle + os.path.sep;
+    subfolder = config.foldername + albumtitle + os.path.sep
 
     # Create subfolder
-    if not (os.path.exists(subfolder)):
-        os.makedirs(subfolder);
+    if not os.path.exists(subfolder):
+        os.makedirs(subfolder)
 
     # Extract image links from html
-    urls = soup.select('.album-view-image-link a');
+    urls = soup.select('.album-view-image-link a')
     for url in urls:
-        url = url['href'];
+        url = url['href']
 
         # Download image
-        url = formatUrl(url);
-        processDirectLink(url, subfolder);
+        url = format_url(url)
+        process_direct_link(url, subfolder)
 
 
-def formatUrl(url):
-    '''
+def format_url(url):
+    """
     Formats url strings by front adding "http:" if needed
     :param url: "//imgur.com/XYZ123"
     :return: "http://imgur.com/XYZ123"
-    '''
-    if (url.startswith("//")):
-        url = "http:" + url;
-    return url;
+    """
+    if url.startswith("//"):
+        url = "http:" + url
+    return url
